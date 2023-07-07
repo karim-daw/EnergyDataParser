ef2a36d17b0acdca4ce50c86d3ae26a378b4adff
Karim Daw <karimdaw91@gmail.com>
2023-07-07 17:43:34 +0100

    add csv writer

---
+++ csv_writer.py
@@ -0,0 +1,17 @@
+import csv
+
+
+def write_table_to_csv(output_file, table_name, values, headers):
+    with open(output_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        # Write table name as a header
+        writer.writerow(['Table Name:', table_name])
+
+        # Write header row
+        writer.writerow(headers)
+
+        # Write data rows
+        for key, row_values in values.items():
+            writer.writerow([key] + row_values)
+
+    print(f"Table information written to '{output_file}'")
