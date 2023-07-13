1ec4dd18275759a333a34eb80e078422ef3ac081
Karim Daw <karimdaw91@gmail.com>
2023-07-13 15:26:52 +0100

    add: some utility helpers

---
+++ utilities.py
@@ -0,0 +1,16 @@
+
+
+from typing import List, Dict
+
+
+def sum_values_by_key(dict_list: List[Dict[str, float]], key: str) -> float:
+    total_sum = 0
+    for dictionary in dict_list:
+        if key in dictionary:
+            total_sum += dictionary[key]
+    return total_sum
+
+
+def display_usage(source: str, name: str, energy_category: str, usage: float):
+    print({'source': source, 'name': name,
+          'energy_category': energy_category, "usage": usage})
