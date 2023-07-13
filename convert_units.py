b106c2f514d3b7927589ce835313059eac9d393b
Karim Daw <karimdaw91@gmail.com>
2023-07-13 10:52:12 +0100

    Add: conversion function from wh to kwh

--- convert_units.py
+++ convert_units.py
@@ -11,3 +11,15 @@ def convert_kwh_to_gj(kwh: float) -> float:
     # Perform the conversion
     gj = kwh * conversion_factor
     return gj
+
+
+def convert_wh_to_kwh(wh: float) -> float:
+    # convertion_factor from Wh to kWh
+    conversion_factor = 0.001
+
+    if wh < 0:
+        raise ValueError("Input wH must be a positive number.")
+
+    # perform the conversion
+    kwh = wh * conversion_factor
+    return kwh
