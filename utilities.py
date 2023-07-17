ddc144da3ce2d416c0864e7126af2ac4f41163c5
Karim Daw <karimdaw91@gmail.com>
2023-07-17 18:53:49 +0100

    Add:  functions to display u values and areas

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 
 
-from typing import List, Dict
+from typing import List, Dict, NamedTuple
+from pprint import pprint
 
 
 def sum_values_by_key(dict_list: List[Dict[str, float]], key: str) -> float:
@@ -13,3 +14,14 @@ def sum_values_by_key(dict_list: List[Dict[str, float]], key: str) -> float:
 
 def display_usage(source: str, energy_category: str, usage: float):
     print({'source': source, 'energy_category': energy_category, "usage": usage})
+
+
+def display_named_tuples(named_tuples: List[NamedTuple]):
+    for named_tuple in named_tuples:
+        print(named_tuple._asdict())
+
+
+def display_named_tuple(named_tuple: NamedTuple):
+    for entry in named_tuple:
+        # print(type(entry))
+        print(entry)
