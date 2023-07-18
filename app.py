98e572eff009349eaeccb0ce759d112f49484f0a
Karim Daw <karimdaw91@gmail.com>
2023-07-18 18:11:27 +0100

    testing ssh

--- app.py
+++ app.py
@@ -15,3 +15,5 @@ output_csv_file = generate_unique_filename(folder_path, output_csv_file_name)
 parsed_values, header_row = parse_html_table(html_file_path, table_names)
 write_table_to_csv(os.path.join(folder_path, output_csv_file),
                    table_names, parsed_values, header_row)
+
+# testing ssh
