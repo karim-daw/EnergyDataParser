093d6215bf4fa6f72c2cf1e19b51e67ed4b88834
Karim Daw <karimdaw91@gmail.com>
2023-07-20 21:12:27 +0100

    Add: known table name keys

---
+++ sim_utilities/table_title_keys.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+from typing import NamedTuple
+
+TableTitle = NamedTuple("TableTitle", [('start_title', str), ('end_title', str)])
+
+# Sample named tuple instances
+ELECTRICAL_ENERGY_ENDUSES = TableTitle(
+    start_title="REPORT- PS-E Energy End-Use Summary for all Electric Meters                                 WEATHER FILE- Los Angeles  CA TMY2",
+    end_title="REPORT- PS-E Energy End-Use Summary for all Fuel Meters                                     WEATHER FILE- Los Angeles  CA TMY2"
+)
