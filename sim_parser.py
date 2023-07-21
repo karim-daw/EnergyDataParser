aa7bd2435212549aafccfeff2f470f57befba882
Karim Daw <karimdaw91@gmail.com>
2023-07-21 11:38:46 +0100

    Add: testing retrieving building sizes and totals

--- sim_parser.py
+++ sim_parser.py
@@ -4,10 +4,15 @@ from pprint import pprint
 from convert_units import convert_kwh_to_gj
 from sim_utilities import table_extractor as te
 from sim_utilities import table_title_keys as ttks
+from sim_utilities import line_title_keys as ltk
 
 
 sim_file_path = "data/JPL LEED Base 10 - Baseline Design.SIM"
 
 
 # get the total electrical energy use from the table
-te.get_totals_from_table(sim_file_path, ttks.ELECTRICAL_ENERGY_ENDUSES)
+# te.get_totals_from_table(sim_file_path, ttks.ELECTRICAL_ENERGY_ENDUSES)
+print(te.extract_data_from_line_in_file(
+    sim_file_path, ltk.BUILING_NUMBER_OF_SPACES))
+print(te.extract_data_from_line_in_file(
+    sim_file_path, ltk.BUILDING_SIZE_TOTALS))
