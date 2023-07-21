2efa46a99a4a2cf72eac15840f5f942493843592
Karim Daw <karimdaw91@gmail.com>
2023-07-21 11:41:26 +0100

    Refactor: decouple line extract from table

--- sim_parser.py
+++ sim_parser.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pprint import pprint
 from convert_units import convert_kwh_to_gj
 from sim_utilities import table_extractor as te
+from sim_utilities import line_extractor as le
 from sim_utilities import table_title_keys as ttks
 from sim_utilities import line_title_keys as ltk
 
@@ -11,8 +12,12 @@ sim_file_path = "data/JPL LEED Base 10 - Baseline Design.SIM"
 
 
 # get the total electrical energy use from the table
-# te.get_totals_from_table(sim_file_path, ttks.ELECTRICAL_ENERGY_ENDUSES)
-print(te.extract_data_from_line_in_file(
+te.get_totals_from_table(sim_file_path, ttks.ELECTRICAL_ENERGY_ENDUSES)
+
+# get number of spaces from the line
+print(le.extract_data_from_line_in_file(
     sim_file_path, ltk.BUILING_NUMBER_OF_SPACES))
-print(te.extract_data_from_line_in_file(
+
+# get building size totals from the line
+print(le.extract_data_from_line_in_file(
     sim_file_path, ltk.BUILDING_SIZE_TOTALS))
