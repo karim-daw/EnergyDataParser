b66defaa692d1121e595c2700af9bb53259875ea
Karim Daw <karimdaw91@gmail.com>
2023-07-21 13:02:55 +0100

    Refactor: decoupled read from pattern finder

--- sim_utilities/line_extractor.py
+++ sim_utilities/line_extractor.py
@@ -3,7 +3,7 @@ import re
 # private function that given a string and a pattern, returns a list of values
 
 
-def __extract_data_from_line(text, pattern):
+def extract_data_from_line(text, pattern):
 
     # Search for the pattern in the text
     match = re.search(pattern, text)
@@ -15,12 +15,3 @@ def __extract_data_from_line(text, pattern):
     else:
         # If no match is found, return None or handle it as you see fit.
         return None
-
-
-def extract_data_from_line_in_file(file_path, desired_data):
-    # Read the contents of the file
-    with open(file_path, 'r') as file:
-        text = file.read()
-
-    # Use the existing function to extract the values
-    return __extract_data_from_line(text, desired_data)
