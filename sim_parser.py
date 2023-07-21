59d6005db3efe539cfc7848ae52b2309e6d12b43
Karim Daw <karimdaw91@gmail.com>
2023-07-21 18:11:56 +0100

    testing: methods for getting out data

--- sim_parser.py
+++ sim_parser.py
@@ -1,7 +1,4 @@
-import re
-import pandas as pd
 from pprint import pprint
-from convert_units import convert_kwh_to_gj
 from sim_utilities import table_extractor as te
 from sim_utilities import line_extractor as le
 from sim_utilities import table_title_keys as ttks
@@ -10,14 +7,30 @@ from sim_utilities import line_title_keys as ltk
 
 sim_file_path = "data/JPL LEED Base 10 - Baseline Design.SIM"
 
+with open(sim_file_path, 'r') as file:
+    text = file.read()
 
-# get the total electrical energy use from the table
-te.get_totals_from_table(sim_file_path, ttks.ELECTRICAL_ENERGY_ENDUSES)
 
 # get number of spaces from the line
-print(le.extract_data_from_line_in_file(
-    sim_file_path, ltk.BUILING_NUMBER_OF_SPACES))
+
 
 # get building size totals from the line
-print(le.extract_data_from_line_in_file(
-    sim_file_path, ltk.BUILDING_SIZE_TOTALS))
+print()
+print("Getting building size data...")
+print(le.extract_data_from_line(
+    text, ltk.BUILDING_SIZE_TOTALS))
+print(le.extract_data_from_line(
+    text, ltk.BUILING_NUMBER_OF_SPACES))
+
+print()
+print("Getting energy data...")
+te.get_data(text, ttks.TOTAL_ENERGY_ENDUSES_BY_FUELTYPE)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.LIGHTS)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.MISC_ELECTRIC)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.SPACE_HEATING)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.SPACE_COOLING)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.PUMPS)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.VENTS_FANS)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.DOMESTIC_HOT_WATER)
+te.get_data(text, ttks.TOTAL_ELECTRICTY_ENDUS_BY_SOURCE, source=ttks.EXTERNAL_EQUIPMENT)
+print()
