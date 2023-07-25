772850678ff87b58abfa0bd04e04349362692b81
Karim Daw <karimdaw91@gmail.com>
2023-07-25 20:47:10 +0100

    Clean up

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ from csv_writer import write_table_to_csv
 from file_utils import generate_unique_filename
 
 # Usage example
+# name of the html file to parse
 html_file_path = 'data/Energy Models_EnergyPlus.htm'
-table_names = ['Site and Source Energy', 'Building Area', "End Uses"]
+
+# names of the tables to parse
+table_names = ['Site and Source Energy', 'Building Area', "End Uses", "Opaque Exterior", "Exterior Fenestration",]
+
+# output file path
 folder_path = 'out/'
 output_csv_file_name = 'output.csv'
+output_path = os.path.join(folder_path, output_csv_file_name)
 
 # Enumerate file if it exists
 output_csv_file = generate_unique_filename(folder_path, output_csv_file_name)
 
+# get table data
 table_data = parse_html_table(html_file_path, table_names)
-write_table_to_csv(os.path.join(folder_path, output_csv_file),
-                   table_data)
 
-# testing ssh
+# output to csv
+write_table_to_csv(output_path, table_data)
