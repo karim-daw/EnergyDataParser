ee16585a35cc65f923e66a259d6d75dc3af17035
Karim Daw <karimdaw91@gmail.com>
2023-07-25 19:13:40 +0100

    Refactor: adapted html parser to ouput dictionary

--- html_parser.py
+++ html_parser.py
@@ -1,32 +1,41 @@
 from bs4 import BeautifulSoup, SoupStrainer
 
 
-def parse_html_table(file_path, table_name):
+def parse_html_table(file_path, table_names):
     with open(file_path) as file:
         # Parse only table tags and b tags
         parse_only = SoupStrainer(['table', 'b'])
-        soup = BeautifulSoup(
-            file, 'lxml', parse_only=parse_only)  # Use lxml parser
+        soup = BeautifulSoup(file, 'lxml', parse_only=parse_only)  # Use lxml parser
 
-        values = {}
-        table_tag = soup.find('b', string=table_name)
-        if table_tag:
-            # Find the table based on the table name
-            table = table_tag.find_next('table')
-            if table:
-                # Find all row tags within the table
-                rows = table.find_all('tr')
+        all_table_data = {}
 
-                # Find header cells in the first row
-                header_cells = rows[0].find_all('td')
-                headers = [cell.get_text(strip=True) for cell in header_cells]
+        for table_name in table_names:
+            # Find all b tags that contain the table names
+            table_tags = soup.find_all('b', string=table_name)
 
-                for row in rows:
-                    cells = row.find_all('td')
-                    row_values = [cell.get_text(strip=True) for cell in cells]
-                    # Store the row values in the dictionary using the first cell as the key
-                    values[row_values[0]] = row_values[1:]
-        else:
-            print("No b tag found")
+            for table_tag in table_tags:
+                # Find the table based on the table name
+                table = table_tag.find_next('table')
+                if table:
+                    # Find all row tags within the table
+                    rows = table.find_all('tr')
 
-        return values, headers
+                    # Find header cells in the first row
+                    header_cells = rows[0].find_all('td')
+                    headers = [cell.get_text(strip=True) for cell in header_cells]
+
+                    # Initialize table-specific data
+                    data = {}
+
+                    for row in rows[1:]:  # Skip the first row as it contains the headers
+                        cells = row.find_all('td')
+                        row_values = [cell.get_text(strip=True) for cell in cells]
+                        # Store the row values in the dictionary using the first cell as the key
+                        data[row_values[0]] = dict(zip(headers, row_values[1:]))
+
+                    # Store the table-specific data in the main dictionary
+                    all_table_data[table_name] = data
+                else:
+                    print(f"No table found for '{table_name}'")
+
+        return all_table_data
