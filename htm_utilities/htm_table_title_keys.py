08fd09ae28aa56195d30c281be96be8ec0e0f67e
Karim Daw <karimdaw91@gmail.com>
2023-08-15 17:58:24 +0100

    adding simple keys

---
+++ htm_utilities/htm_table_title_keys.py
@@ -0,0 +1,6 @@
+
+SITE_AND_SOURCE_ENERGY = 'Site and Source Energy'
+BUILDING_AREA = 'Building Area'
+END_USES = "End Uses"
+OPAQUE_EXTERIOR = "Opaque Exterior"
+EXTERIOR_FENESTRATION = "Exterior Fenestration"
