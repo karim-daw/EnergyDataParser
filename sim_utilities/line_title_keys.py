70711feb1b22d6697507f358f27fb8f0daacd87e
Karim Daw <karimdaw91@gmail.com>
2023-08-17 15:43:05 +0100

    Refactor: Rename

--- sim_utilities/line_title_keys.py
+++
@@ -1,4 +0,0 @@
-
-# Sample named tuple instances
-BUILDING_SIZE_TOTALS = r'BUILDING TOTALS\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)'
-BUILING_NUMBER_OF_SPACES = r'NUMBER OF SPACES\s+(\d+)\s+EXTERIOR\s+(\d+)\s+INTERIOR\s+(\d+)'
